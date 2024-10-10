d37f9db1c78b6e1042395816ed923eae698ad487
Omar <shikzadayevomar@gmail.com>
2024-10-10 12:52:43 +0400

    10.10

--- projects/main.cpp
+++ projects/main.cpp
@@ -9,8 +9,10 @@ bool IsAfternoonTime(const chron::Time& time) {
 int main() {
     chron::Time t1 (10,19,10);
      chron::Time t2 (10,19,10);
+     chron::Time t3;
+     std::cin>>t3;
      std::cout<<t1-t2<<"\n";
-     std::cout<<t1<<"\n";
+     std::cout<<t3<<"\n";
     t2.AddSeconds(2300);
     std::cout<<t2.GetHours()<<"\n";
     std::cout<<t2.GetMinutes()<<"\n";
